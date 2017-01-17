b8958e4eb65d533ae35e2d7bf27d2c0e78582852
Nick C <me@nickc.io>
2017-01-17 16:10:04 -0500

    Remove unused code from index controller

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -4,14 +4,13 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class IndexController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         return $this->render('default/index.html.twig');
     }
