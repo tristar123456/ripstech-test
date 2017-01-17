33563551826cd2d40c222442db81280afc6f921e
Nick C <me@nickc.io>
2017-01-17 16:17:08 -0500

    Add points controller with get/post endpoints

---
+++ src/AppBundle/Controller/PointsController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use DateTime;
+use AppBundle\Entity\Point;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+
+class PointsController extends FOSRestController
+{
+    public function optionsPointsAction()
+    {
+        $options = [
+            'endpoints' => [
+                'GET' => [
+                    '/users' => [
+                    
+                    ],
+                ],
+                'POST' => [
+                    '/users' => [
+                    
+                    ],
+                ],
+            ],
+        ];
+        $view = $this->view($options, 200);
+
+        return $this->handleView($view);
+    }
+
+    public function getPointsAction()
+    {
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Point');
+        $points = $repository->findAll();
+        $view = $this->view($points, 200);
+
+        return $this->handleView($view);
+    }
+    
+    public function postPointsAction(Request $request)
+    {
+        $point = new Point();
+
+        $point->setLat($request->request->get('lat'));
+        $point->setLong($request->request->get('long'));
+        $point->setCreatedAt(new DateTime());
+
+        $validator = $this->get('validator');
+        $errors = $validator->validate($point);
+
+        if (count($errors) > 0) {
+            $view = $this->view($errors, 400);
+
+            return $this->handleView($view);
+        }
+
+        $view = $this->view($point, 200);
+
+        return $this->handleView($view);
+    }
+}
