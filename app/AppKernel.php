b330db90ec219a669a6327646c8dce93c7dd8cc0
Nick C <me@nickc.io>
2017-01-17 17:36:40 -0500

    Add geoip library

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\RestBundle\FOSRestBundle(),
+            new Maxmind\Bundle\GeoipBundle\MaxmindGeoipBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
