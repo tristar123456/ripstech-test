956e8bc6792afc1750a1753b1d791dd8439bafa5
Nick C <me@nickc.io>
2017-01-17 23:28:49 -0500

    Add data polling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,36 @@
-import React, {Component, PropTypes} from 'react';
+import React, {PropTypes} from 'react';
 import {Map, TileLayer} from 'react-leaflet';
+import asyncPoll from 'react-async-poll';
 
-class App extends Component {
-  static propTypes = {
-    points: PropTypes.arrayOf(PropTypes.shape({
-      id: PropTypes.number,
-      lat: PropTypes.number,
-      long: PropTypes.number,
-      icon: PropTypes.string,
-    })),
-  };
+function App(props = {points: []}) {
+  const position = {lat: 45, lng: 0};
+  const points = props.points || [];
 
-  static defaultProps = {
-    points: [],
-  };
-
-  render() {
-    const position = {lat: 45, lng: 0};
-
-    return (
-      <Map center={position} zoom={3}>
-        <TileLayer
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-      </Map>
-    );
-  }
+  return (
+    <Map center={position} zoom={3}>
+      <TileLayer
+        url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </Map>
+  );
 }
 
-export default App;
+App.propTypes = {
+  points: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    lat: PropTypes.number,
+    long: PropTypes.number,
+    icon: PropTypes.string,
+  })),
+};
+
+async function onPoll(props) {
+  const options = {method: 'GET'};
+  const response = await fetch('/points', options);
+  const data = await response.json();
+
+  return new App({...props, ...{points: data}});
+}
+
+export default asyncPoll(2 * 1000, onPoll)(App);
