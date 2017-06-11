cd01d09e647bc37d7616ebe00a2e2bca316221c0
FMK <gitlab@rofln.de>
2017-06-11 21:56:11 +0200

    Add c3js chart to display fetched data

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,5 +2,6 @@ import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
 import App from './app';
+import './chart';
 
 render(<App />, document.getElementById('app'));
